4fe378d4b099a03080c173bd892bc57d8f6612b7
mohammad-ebrahimi-it <mohammadebe@gmail.ci=om>
2021-11-28 20:16:53 +0330

    create model post

---
+++ src/main/java/com/shop/shopping/services/Posts.java
@@ -0,0 +1,59 @@
+package com.shop.shopping.services;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Posts {
+
+    private Long id;
+    private String title;
+    private String cover;
+    private LocalDateTime CreatedAt;
+    private LocalDateTime UpdateAt;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return CreatedAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        CreatedAt = createdAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return UpdateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        UpdateAt = updateAt;
+    }
+}
