ece7a45cbc850fe4a4a13df96d93fde5b27f5bb6
mohammad-ebrahimi-it <mohammadebe@gmail.com>
2022-03-30 22:30:43 +0430

    add spring security and add jwt token

--- src/main/java/com/shop/shopping/config/Config.java
+++ src/main/java/com/shop/shopping/config/Config.java
@@ -4,6 +4,9 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class Config {
@@ -12,6 +15,23 @@ public class Config {
     public MessageSourceAccessor messageSourceAccessor(MessageSource messageSource) {
         return new MessageSourceAccessor(messageSource);
     }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new UserDetailsService() {
+//            @Override
+//            public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String username) {
+//                return null;
+//            }
+//        };
+//    }
+
+
+
 
 
 }
