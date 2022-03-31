8dd675dd0efe700b336725ea92aedb35f1e08076
mohammad-ebrahimi-it <mohammadebe@gmail.com>
2022-03-31 20:41:54 +0430

    add role adn customer authentication

---
+++ src/main/java/com/shop/shopping/model/Role.java
@@ -0,0 +1,41 @@
+package com.shop.shopping.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.*;
+
+@Entity
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+
+public class Role {
+
+    private Long id;
+    private String name;
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
