5b33594b045720804f7a1a726cfe1d0a42267f35
mohammad-ebrahimi-it <mohammadebe@gmail.com>
2022-04-03 22:41:10 +0430

    Add:  role request for project

---
+++ src/main/java/com/shop/shopping/dto/requests/RoleRequest.java
@@ -0,0 +1,23 @@
+package com.shop.shopping.dto.requests;
+
+public class RoleRequest {
+
+    private String name;
+    private String persianName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPersianName() {
+        return persianName;
+    }
+
+    public void setPersianName(String persianName) {
+        this.persianName = persianName;
+    }
+}
