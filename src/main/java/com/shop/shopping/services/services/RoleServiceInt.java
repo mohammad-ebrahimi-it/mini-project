304d2a3dad55f549afca66da78eed9cc822423ba
mohammad-ebrahimi-it <mohammadebe@gmail.com>
2022-04-03 22:40:24 +0430

    Add: classes role service and add functions

---
+++ src/main/java/com/shop/shopping/services/services/RoleServiceInt.java
@@ -0,0 +1,15 @@
+package com.shop.shopping.services.services;
+
+import com.shop.shopping.dto.requests.RoleRequest;
+import com.shop.shopping.dto.response.RoleResponse;
+import com.shop.shopping.model.role.Roles;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface RoleServiceInt {
+     Roles addRole(RoleRequest roleRequest);
+
+     Page<Roles> getAll(Pageable pageable);
+
+     RoleResponse getById(Long id);
+}
