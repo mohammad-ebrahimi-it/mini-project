eb7082b38e856ea22b9889a739a9e7a5185cac32
mohammad-ebrahimi-it <mohammadebe@gmail.com>
2022-04-06 22:13:57 +0430

    change :  change athentication and change validation

--- src/main/java/com/shop/shopping/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/shop/shopping/filter/CustomAuthorizationFilter.java
@@ -6,7 +6,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,7 +20,6 @@ import java.util.*;
 
 import static java.util.Arrays.stream;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -29,41 +27,37 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getParameter("password") == null || request.getParameter("username") == null){
-            this.objectMapper(request, response);
-        }
-        if (request.getServletPath().equals("/api/login") || request.getServletPath().equals("/api/token/refresh/**")){
-            filterChain.doFilter(request,response);
-            Map<String ,String > error = new HashMap<>();
-            error.put("error","Unauthorized");
+        if (request.getServletPath().equals("/api/login") || request.getServletPath().equals("/api/token/refresh/**")) {
+            filterChain.doFilter(request, response);
+            Map<String, String> error = new HashMap<>();
+            error.put("error", "Unauthorized");
 
             new ObjectMapper().writeValue(response.getOutputStream(), error);
-        }
-        else {
+        } else {
             String authorizationHeader = request.getHeader(AUTHORIZATION);
             if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
                 try {
-                    String token  = authorizationHeader.substring("Bearer ".length());
+                    String token = authorizationHeader.substring("Bearer ".length());
                     Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
                     JWTVerifier verifier = JWT.require(algorithm).build();
                     DecodedJWT decodedJWT = verifier.verify(token);
                     String username = decodedJWT.getSubject();
                     String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
                     Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-                    stream(roles).forEach(role ->{
+                    stream(roles).forEach(role -> {
                         authorities.add(new SimpleGrantedAuthority(role));
                     });
                     UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                     filterChain.doFilter(request, response);
-                } catch (Exception exception){
+                } catch (Exception exception) {
                     log.error("Error logging in {}", exception.getMessage());
                     response.setHeader("error", exception.getMessage());
                     response.setStatus(FORBIDDEN.value());
 //                    response.sendError(FORBIDDEN.value());
-                    Map<String ,String > error = new HashMap<>();
+                    Map<String, String> error = new HashMap<>();
                     error.put("error_message", exception.getMessage());
-                    error.put("error_code", FORBIDDEN.value()+"");
+                    error.put("error_code", FORBIDDEN.value() + "");
                     error.put("date", new Date().toString());
                     response.setContentType(APPLICATION_JSON_VALUE);
                     new ObjectMapper().writeValue(response.getOutputStream(), error);
@@ -73,25 +67,6 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
                 filterChain.doFilter(request, response);
             }
         }
-
-    }
-    private ObjectMapper objectMapper(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Map<String ,String> error = new HashMap<>();
-        error.put("date", new Date().toString());
-        ObjectMapper objectMapper = new ObjectMapper();
-        response.setStatus(FORBIDDEN.value());
-        response.setContentType(APPLICATION_JSON_VALUE);
-        error.put("status", HttpStatus.UNAUTHORIZED.toString());
-
-        if (request.getParameter("username") == null){
-            error.put("error_message", "Username is required");
-            objectMapper.writeValue(response.getOutputStream(), error);
-        }
-        if (request.getParameter("password") == null){
-            error.put("error_message", "Password is required");
-            objectMapper.writeValue(response.getOutputStream(), error);
-        }
-        return objectMapper;
 
     }
 }
--- src/main/java/com/shop/shopping/filter/CustomerAuthenticationFilter.java
+++ src/main/java/com/shop/shopping/filter/CustomerAuthenticationFilter.java
@@ -2,10 +2,8 @@ package com.shop.shopping.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -20,7 +18,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.rmi.ServerError;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -43,11 +40,7 @@ public class CustomerAuthenticationFilter extends UsernamePasswordAuthentication
         String password = request.getParameter("password");
         log.info("Username is: {}", username);
         log.info("Password is: {}", password);
-        if (username == null || password == null) {
-
-
-//            return (Authentication) this.objectMapper(request);
-        }
+        response.setContentType(APPLICATION_JSON_VALUE);
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
         return authenticationManager.authenticate(authenticationToken);
     }
@@ -56,24 +49,15 @@ public class CustomerAuthenticationFilter extends UsernamePasswordAuthentication
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
         User user = (User) authentication.getPrincipal();
         Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
-        String access_token = JWT.create()
-                .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
-                .withIssuer(request.getRequestURL().toString())
-                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
-                .sign(algorithm);
+        String access_token = JWT.create().withSubject(user.getUsername()).withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000)).withIssuer(request.getRequestURL().toString()).withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList())).sign(algorithm);
 
-        String refresh_token = JWT.create()
-                .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
-                .withIssuer(request.getRequestURL().toString())
-                .sign(algorithm);
+        String refresh_token = JWT.create().withSubject(user.getUsername()).withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000)).withIssuer(request.getRequestURL().toString()).sign(algorithm);
 
         /*response.setHeader("access_token", access_token);
         response.setHeader("refresh_token", refresh_token);*/
-        Map<String ,String> tokens = new HashMap<>();
-        tokens.put("access_token" ,access_token);
-        tokens.put("refresh_token" ,refresh_token);
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+        tokens.put("refresh_token", refresh_token);
 
         response.setContentType(APPLICATION_JSON_VALUE);
         new ObjectMapper().writeValue(response.getOutputStream(), tokens);
